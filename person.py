12c6c46c6783272cc133566cc25a54e24b32fcdf
JKumar1997 <jason.kumar@student.uts.edu.au>
2020-03-24 17:42:16 +1100

    Intitial commit
    Age 0-1.
    Creates the random player data
    To-do list in Person.py

---
+++ person.py
@@ -0,0 +1,98 @@
+import random
+
+print("Welcome to BytLife! A Python clone based on the popular App, BitLife! All rights to the original owners, "
+      "CandyCane LLC")
+year = 0
+
+
+#   To do next:
+#   - Birthday/Star Sign
+#   - Emotions that update each time you age
+#   - Parents
+#   - Location where you were concieved.
+#   - Grab names from an API vs. making them manually.
+#   - Reduce repetitive code
+
+class Person:
+    def __init__(self, sex, first_name, last_name, age, ethnicity):
+        self.sex = sex
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.ethnicity = ethnicity
+
+    def determine_sex(self):
+        if random.randint(0, 1) == 0:
+            self.sex = "male"
+        else:
+            self.sex = "female"
+        return self.sex
+
+    def determine_name(self):
+        if self.sex == "male":
+            first = ("Robert", "Sam", "Joe")
+            self.first_name = random.choice(first)
+        if self.sex == "female":
+            first = ("Joanne", "Laura", "Aubry")
+            self.first_name = random.choice(first)
+        return self.first_name
+
+    def determine_surname(self):
+        last = ("Jones", "Peterson", "Smith")
+        self.last_name = random.choice(last)
+        return self.last_name
+
+    def determine_age(self):
+        self.age = year
+        return self.age
+
+    def determine_ethnicity(self):
+        countries = {
+            "Australia": "Australian",
+            "Germany": "German",
+            "Italy": "Italian",
+            "France": "French",
+            "China": "Chinese",
+            "Sweden": "Swedish"
+        }
+        self.ethnicity = random.choice(list(countries.values()))
+        return self.ethnicity
+
+
+class Emotions(Person):
+    pass
+
+
+#   Underline the word so it will always be equal to the length of it.
+#   Use: u_word("=", "Hello") or the word param w/ a function that returns a string.
+def underline_word(placeholder, word):
+    word = placeholder * len(word)
+    return word
+
+
+#  Instantiate the class Person and start playing.
+def client():
+    #   Uh. . .
+    p = Person("", "", "", "", "")
+    sex = p.determine_sex()
+    name = p.determine_name()
+    surname = p.determine_surname()
+    ethnicity = p.determine_ethnicity()
+    age = p.determine_age()
+    begin = input("Age? Press A\n")
+
+    start = 0
+    if start <= 0 and (begin == "a" or begin == "A"):
+        display_age = "Age: {} Years".format(str(int(age) + 1))
+        print(display_age)
+        print(underline_word("=", display_age))
+        print(f'I am a {ethnicity} {sex}. I was conceived at a [LOCATION].\n'
+              f'\n'
+              f'            My birthday is MONTH DAY. I am a STAR SIGN.\n'
+              f'\n'
+              f'            My name is {name} {surname}.\n'
+              f'            My father is FATHER {surname}, a JOB (age (AGE))\n'
+              f'            My mother is MOTHER {surname}, a JOB (age (AGE))')
+
+
+client()
